4a512f1e896386ac84d646a1532f17e7ed9af794
RedWheeler <ethannetsch@gmail.com>
2020-12-06 16:42:47 -0500

    Add second servo control code to server.py and index.html

--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
 import picamera
 import pigpio
+import io
 
 
 class Camera:
@@ -9,12 +10,14 @@ class Camera:
     SERVO_2 = 18
 
     def __init__(self):
-        self.__image = open('stream.jpg', 'wb')
+        self.__stream = io.BytesIO()
         self.__camera = picamera.PiCamera()
         # Servo init
         self.__pi = pigpio.pi()
-        self.__servo1_rotation = 0
-        self.__servo2_rotation = 0
+        self.__servo1_rotation = 90
+        self.servo1_rotation = 90
+        self.__servo2_rotation = 90
+        self.servo2_rotation = 90
 
     # Increment servo 1's rotation by the specified angle
     def rotate_servo1(self, angle):
@@ -41,6 +44,7 @@ class Camera:
     @servo1_rotation.setter
     def servo1_rotation(self, angle):
         if self.MIN_ANGLE < angle < self.MAX_ANGLE:
+            self.__servo1_rotation = angle
             self.__pi.set_servo_pulsewidth(self.SERVO_1, 500 + (angle / 180) * 2000)
         else:
             raise ValueError(f"Value must be between {self.MIN_ANGLE} and {self.MAX_ANGLE}")
@@ -52,11 +56,15 @@ class Camera:
     @servo2_rotation.setter
     def servo2_rotation(self, angle):
         if self.MIN_ANGLE < angle < self.MAX_ANGLE:
+            self.__servo2_rotation = angle
             self.__pi.set_servo_pulsewidth(self.SERVO_2, 500 + (angle / 180) * 2000)
         else:
             raise ValueError(f"Value must be between {self.MIN_ANGLE} and {self.MAX_ANGLE}")
 
     def get_frame(self):
-        # Process and return frame
-        self.__camera.capture(self.__image)
-        return self.__image
+        # Clear stream
+        self.__stream.seek(0)
+        self.__stream.truncate(0)
+        # Read new image into stream
+        self.__camera.capture(self.__stream, format='jpeg')
+        return self.__stream
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
-from flask import Flask, request, render_template, Response, send_file
+from flask import Flask, request, render_template, Response
 from camera import Camera
-from subprocess import Popen, PIPE
-app = Flask(__name__)
 
+app = Flask(__name__)
 camera = Camera()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 def gen():
     while True:
@@ -15,16 +16,21 @@ def gen():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
 @app.route('/camera')
 def camera():
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/rotate', methods=['POST'])
-def rotate():
+
+@app.route('/rotate_servo1', methods=['POST'])
+def rotate_servo1():
     rotation = request.form['rotation']
     camera.rotate_servo1(rotation)
-    return 'OK'
+    return 'OK', 202
 
-@app.route('/sweep', methods=['POST'])
-def sweep():
-    return 'OK'
+
+@app.route('/rotate_servo2', methods=['POST'])
+def rotate_servo2():
+    rotation = request.form['rotation']
+    camera.rotate_servo2(rotation)
+    return 'OK', 202
