d0edadb01118ab4dc69f2bbbab8e2672ef2d9458
RedWheeler <ethannetsch@gmail.com>
2020-12-05 20:44:47 -0500

    Move servo logic to Camera class

--- camera.py
+++ camera.py
@@ -1,11 +1,62 @@
 import picamera
+import pigpio
+
 
 class Camera:
+    MIN_ANGLE = 50
+    MAX_ANGLE = 140
+    SERVO_1 = 17
+    SERVO_2 = 18
+
     def __init__(self):
-        self.image = open('stream.jpg', 'wb')
-        self.camera = picamera.PiCamera()
+        self.__image = open('stream.jpg', 'wb')
+        self.__camera = picamera.PiCamera()
+        # Servo init
+        self.__pi = pigpio.pi()
+        self.__servo1_rotation = 0
+        self.__servo2_rotation = 0
+
+    # Increment servo 1's rotation by the specified angle
+    def rotate_servo1(self, angle):
+        if self.__servo1_rotation + angle > self.MAX_ANGLE:
+            self.servo1_rotation = self.MAX_ANGLE
+        elif self.__servo1_rotation + angle < self.MIN_ANGLE:
+            self.servo1_rotation = self.MIN_ANGLE
+        else:
+            self.servo1_rotation = self.servo1_rotation + angle
+
+    # Increment servo 2's rotation by the specified angle
+    def rotate_servo1(self, angle):
+        if self.__servo2_rotation + angle > self.MAX_ANGLE:
+            self.servo2_rotation = self.MAX_ANGLE
+        elif self.__servo2_rotation + angle < self.MIN_ANGLE:
+            self.servo2_rotation = self.MIN_ANGLE
+        else:
+            self.servo2_rotation = self.servo2_rotation + angle
+
+    @property
+    def servo1_rotation(self):
+        return self.__servo1_rotation
+
+    @servo1_rotation.setter
+    def servo1_rotation(self, angle):
+        if self.MIN_ANGLE < angle < self.MAX_ANGLE:
+            self.__pi.set_servo_pulsewidth(self.SERVO_1, 500 + (angle / 180) * 2000)
+        else:
+            raise ValueError()
+
+    @property
+    def servo2_rotation(self):
+        return self.__servo2_rotation
+
+    @servo2_rotation.setter
+    def servo2_rotation(self, angle):
+        if self.MIN_ANGLE < angle < self.MAX_ANGLE:
+            self.__pi.set_servo_pulsewidth(self.SERVO_2, 500 + (angle / 180) * 2000)
+        else:
+            raise ValueError
 
     def get_frame(self):
         # Process and return frame
-        self.camera.capture(self.image)
-        return self.image
+        self.__camera.capture(self.__image)
+        return self.__image
--- server.py
+++ server.py
@@ -3,14 +3,13 @@ from camera import Camera
 from subprocess import Popen, PIPE
 app = Flask(__name__)
 
-# Start controller process
-controller = Popen("python controller.py", stdin=PIPE, stdout=PIPE)
+camera = Camera()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def gen(camera):
+def gen():
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
@@ -18,16 +17,14 @@ def gen(camera):
 
 @app.route('/camera')
 def camera():
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/rotate', methods=['POST'])
 def rotate():
-    rotation = bytes("rotate="+request.form['rotation'], 'utf-8')
-    controller.stdin.write(rotation)
+    rotation = request.form['rotation']
+    camera.rotate_servo1(rotation)
     return 'OK'
 
 @app.route('/sweep', methods=['POST'])
 def sweep():
-    controller.stdin.write(b'sweep')
     return 'OK'
